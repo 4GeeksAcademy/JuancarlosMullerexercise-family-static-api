a1f3cbf4152962a10194a98ffd41566f5b000510
Juancarlos Muller Baeza <118220005+JuancarlosMuller@users.noreply.github.com>
2023-09-29 17:57:55 +0000

    Falla en inicializacion

--- src/app.py
+++ src/app.py
@@ -26,17 +26,75 @@ def sitemap():
     return generate_sitemap(app)
 
 @app.route('/members', methods=['GET'])
-def handle_hello():
+def get_all_members():
+    try:
+   
+        member = jackson_family.get_all_members()
 
-    # this is how you can use the Family datastructure by calling its methods
-    members = jackson_family.get_all_members()
-    response_body = {
-        "hello": "world",
-        "family": members
-    }
+      
+        if not isinstance(member, list):
+            raise APIException('Internal server error', status_code=500)
+
+        response_body = {
+            "family": member
+        }
+
+        return jsonify(response_body), 200
+    except Exception as e:
+     
+        return jsonify({'error': str(e)}), 500
 
 
-    return jsonify(response_body), 200
+
+@app.route('/member', methods=['POST'])
+def add_member():
+    try:
+        data = request.json
+
+        if not data or "first_name" not in data or "age" not in data or "lucky_numbers" not in data:
+            return jsonify({"error": "Invalid request data"}), 400
+
+        new_member = {
+            "first_name": data["first_name"],
+            "age": data["age"],
+            "lucky_numbers": data.get("lucky_numbers", []),
+        }
+
+        if "id" in data:
+            new_member["id"] = data["id"]
+
+        jackson_family.add_member(new_member)
+        
+        return jsonify({"message": "Member added successfully"}), 200
+
+    except Exception as e:
+        return jsonify({"error": "Internal server error"}), 500
+
+
+@app.route('/member/<int:member_id>', methods=['GET'])
+def get_member_by_id(member_id):
+    member = jackson_family.get_member(member_id)
+
+    if member is None:
+        return jsonify({"error": "Member not found"}), 404
+
+    return jsonify(member), 200
+
+@app.route('/member/<int:member_id>', methods=['DELETE'])
+def delete_member(member_id):
+    try:
+        deleted = jackson_family.delete_member(member_id)
+
+        if deleted:
+            return jsonify({"done": True}), 200
+        else:
+            return jsonify({"error": "Member not found"}), 400
+
+    except Exception as e:
+        return jsonify({"error": "Internal server error"}), 500
+
+
+
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
